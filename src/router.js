fbc6776e26a0833fed0d0a406ad8c0fbcdb7a249
Mike <mikehammond32@gmail.com>
2019-08-29 20:27:29 -0400

    logout added

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
 
 Vue.use(Router)
 
@@ -24,6 +25,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     { path: "/signup", name: "signup", component: Signup },
-    { path: "/login", name: "login", component: Login }
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout }
   ]
 })
