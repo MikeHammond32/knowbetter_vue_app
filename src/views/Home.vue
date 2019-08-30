687f956b7ff0ec68e22dc67529df9ec9686b4503
Mike <mikehammond32@gmail.com>
2019-08-29 21:55:21 -0400

    index template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Clients</h1>
+    <div v-for="client in clients">
+      <h2>{{ photo.name }}</h2>
+      <img v-bind:src="photo.url">
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +14,20 @@
 </style>
 
 <script>
+import axios from "axios";
+
+
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!"
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/clients").then(response => {
+      this.photos = response.data;
+    }); 
+  },
   methods: {}
 };
 </script>
