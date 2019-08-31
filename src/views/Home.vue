0d394d588ea7982f29178e498cd63e138c39d323
Mike <mikehammond32@gmail.com>
2019-08-30 20:15:26 -0400

    show and create templates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="home">
+  <div class="container">
+        <h1>New Client</h1>
+    <div>
+      Name: <input type="text" v-model="newClientName">
+      Width: <input type="text" v-model="newClientWidth">
+      Height: <input type="text" v-model="newClientHeight">
+      <button v-on:click="createClient()">Create client</button>
+    </div>
     <h1>All Clients</h1>
     <div v-for="client in clients">
-      <h2>{{ photo.name }}</h2>
-      <img v-bind:src="photo.url">
-      <p>Width: {{ photo.width }}</p>
-      <p>Height: {{ photo.height }}</p>
+      <h2>{{ client.name }}</h2>
+      <img v-bind:src="client.url">
+      <p>Width: {{ client.width }}</p>
+      <p>Height: {{ client.height }}</p>
     </div>
   </div>
 </template>
@@ -17,17 +24,47 @@
 import axios from "axios";
 
 
+<script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      photos: [],
+      currentPhoto: {},
+      newPhotoName: "",
+      newPhotoWidth: "",
+      newPhotoHeight: ""
     };
   },
   created: function() {
-    axios.get("/api/clients").then(response => {
+    axios.get("/api/photos").then(response => {
       this.photos = response.data;
-    }); 
+    });
   },
-  methods: {}
+  methods: {
+    createPhoto: function() {
+      var params = {
+        name: this.newPhotoName,
+        width: this.newPhotoWidth,
+        height: this.newPhotoHeight
+      };
+      axios.post("/api/photos", params).then(response => {
+        this.photos.push(response.data);
+        this.newPhotoName = "";
+        this.newPhotoWidth = "";
+        this.newPhotoHeight = "";
+      });
+    },
+    showPhoto: function(photo) {
+      if (this.currentPhoto === photo) {
+        this.currentPhoto = {};
+      } else {
+        this.currentPhoto = photo;
+      }
+    }
+  }
 };
 </script>
+
+
