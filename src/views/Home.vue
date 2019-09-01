49f0e777eaf28b90b610a17c86a4d123563207b5
Mike <mikehammond32@gmail.com>
2019-09-01 09:14:57 -0400

    updated template to fit clients

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,27 @@
   <div class="container">
         <h1>New Client</h1>
     <div>
-      Name: <input type="text" v-model="newClientName">
-      Width: <input type="text" v-model="newClientWidth">
-      Height: <input type="text" v-model="newClientHeight">
-      <button v-on:click="createClient()">Create client</button>
+      First Name: <input type="text" v-model="newUserFirstName">
+      Last Name: <input type="text" v-model="newUserLastName">
+      Phone Number: <input type="text" v-model="newUserPhoneNumber">
+      Location: <input type="text" v-model="newUserLocation">
+      Bio: <input type="text" v-model="newUserBio">
+      Profession: <input type="text" v-model="newUserProfession">
+      <button v-on:click="createUser()">Create user</button>
     </div>
-    <h1>All Clients</h1>
-    <div v-for="client in clients">
-      <h2>{{ client.name }}</h2>
-      <img v-bind:src="client.url">
-      <p>Width: {{ client.width }}</p>
-      <p>Height: {{ client.height }}</p>
+    <h1>All Users</h1>
+    <div v-for="user in users">
+      <h2>{{ user.name }}</h2>
+      <img v-bind:src="user.url">
+      <p>FirstName: {{ user.FirstName }}</p>
+      <p>LastName: {{ user.LastName }}</p>      
+      <p>PhoneNumber: {{ user.PhoneNumber }}</p>
+      <p>Location: {{ user.Location }}</p>
+      <p>Bio: {{ user.Bio }}</p>
+      <p>Profession: {{ user.Profession }}</p>
     </div>
   </div>
 </template>
-
-<style>
-</style>
-
-<script>
-import axios from "axios";
 
 
 <script>
@@ -30,41 +31,64 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      photos: [],
-      currentPhoto: {},
-      newPhotoName: "",
-      newPhotoWidth: "",
-      newPhotoHeight: ""
+      users: [],
+      currentUser: {},
+      newUserFirstName: "",
+      newUserLastName: "",
+      newUserPhoneNumber: "",
+      newUserLocation: "",
+      newUserBio: "",
+      newUserProfession: ""
     };
   },
   created: function() {
-    axios.get("/api/photos").then(response => {
-      this.photos = response.data;
+    axios.get("/api/users").then(response => {
+      this.users = response.data;
     });
   },
   methods: {
-    createPhoto: function() {
+    createUser: function() {
       var params = {
-        name: this.newPhotoName,
-        width: this.newPhotoWidth,
-        height: this.newPhotoHeight
+        FirstName: this.newUserName,
+        LastName: this.newUserWidth,
+        PhoneNumber: this.newUserPhoneNumber,
+        Location: this.newUserLocation,
+        Bio: this.newUserBio,
+        Profession: this.newUserProfession
       };
-      axios.post("/api/photos", params).then(response => {
-        this.photos.push(response.data);
-        this.newPhotoName = "";
-        this.newPhotoWidth = "";
-        this.newPhotoHeight = "";
+      axios.post("/api/users", params).then(response => {
+        this.users.push(response.data);
+        this.newUserFirstName = "";
+        this.newUserLastName = "";
+        this.newUserPhoneNumber = "";
+        this.newUserLocation = "";
+        this.newUserBio = "";
+        this.newUserProfession = "";
       });
     },
-    showPhoto: function(photo) {
-      if (this.currentPhoto === photo) {
-        this.currentPhoto = {};
+    showUser: function(user) {
+      if (this.currentUser === user) {
+        this.currentUser = {};
       } else {
-        this.currentPhoto = photo;
+        this.currentUser = user;
       }
+    },
+    updateUser: function(user) {
+      var params = {
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        PhoneNumber: user.PhoneNumber,
+        Location: user.Location,
+        Bio: user.Bio,
+        Profession: user.Profession
+      };
+      axios
+        .patch("/api/users/" + user.id, params)
+        .then(response => {
+          this.currentUser = {};
+        });
     }
   }
 };
 </script>
-
 
